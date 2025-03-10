13b320443bf2ac2e5bcffe0cce26dc4b5241e84b
elghemary1 <farah.elghemary@outlook.com>
2025-03-10 04:41:06 +0100

    Update 02_SpaceShipInterface.ino

--- Starter Kit Projects/02_SpaceShipInterface.ino
+++ Starter Kit Projects/02_SpaceShipInterface.ino
@@ -1,1 +1,34 @@
-test
+int switchState = 0;
+int redLed1 = 5;
+int redLed2 = 4;
+int greenLed = 3;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(switchState, INPUT);
+  pinMode(redLed1, OUTPUT);
+  pinMode(redLed2, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+
+}
+
+void loop() {
+  switchState = digitalRead(2);
+  if (switchState == LOW){
+    digitalWrite(greenLed, HIGH);
+    digitalWrite(redLed2, LOW);
+    digitalWrite(redLed1, LOW);
+  }
+
+  else {
+    digitalWrite(greenLed, LOW);
+    digitalWrite(redLed2, LOW);
+    digitalWrite(redLed1, HIGH);
+    delay(250);
+
+    digitalWrite(redLed2, HIGH);
+    digitalWrite(redLed1, LOW);
+    delay(250);
+  }
+  
+}
